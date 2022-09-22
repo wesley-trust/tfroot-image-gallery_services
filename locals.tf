a5c99dda6b74e8009865d4e6194ad7fa8264a097
wesley-trust <wesley-trust@users.noreply.github.com>
2022-09-22 17:05:48 +0100

    Update variables

--- locals.tf
+++ locals.tf
@@ -1,8 +1,9 @@
 # Calculate local variables
 locals {
   # Service functions and concatenations
-  service_location_prefix    = replace(var.service_location, "/[a-z[:space:]]/", "")
-  service_environment_prefix = substr(var.service_environment, 0, 1)
+  service_location           = lookup(var.service_location, "${terraform.workspace}", null)
+  service_location_prefix    = replace(local.service_location, "/[a-z[:space:]]/", "")
+  service_environment_prefix = substr(terraform.workspace, 0, 1)
 
   # Resource functions and concatenations
   resource_name_prefix = lookup(var.resource_name, var.service_name, null)
